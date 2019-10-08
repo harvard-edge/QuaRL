f854946684f6dd5dac60f6c8e392130bfd22ef84
Sharad24 <sharadchitlangia24sc@gmail.com>
2019-10-08 10:58:59 +0000

    Add freeze script

---
+++ quant-scripts/freeze.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+base="$5/saved/$4/$1/$2"
+/home/sharadchitlangia/.conda/envs/tq/bin/freeze_graph \
+--input_graph="$base.pb" \
+--input_checkpoint="$base.ckpt" \
+--output_graph="$5/frozen/$4/$1/$2.pb" --output_node_names="$3"
+
